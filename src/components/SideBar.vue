0368fccf8c42204d8a562cb1896ce9744d48714d
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-07-20 18:25:03 -0300

    atualizado sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -12,226 +12,18 @@
         </a>
       </li><!-- End Dashboard Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+      <li class="nav-item" v-for="item in menu">
+        <a class="nav-link collapsed" :data-bs-target="`#${item.nome}-nav`" data-bs-toggle="collapse" href="">
+          <i class="bi bi-menu-button-wide"></i><span>{{ item.nome }}</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <router-link to="login">
-              <i class="bi bi-circle"></i><span>Alertas</span>
-              </router-link>
-          </li>
-          <li>
-            <a href="components-accordion.html">
-              <i class="bi bi-circle"></i><span>Accordion</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-badges.html">
-              <i class="bi bi-circle"></i><span>Badges</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-breadcrumbs.html">
-              <i class="bi bi-circle"></i><span>Breadcrumbs</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-buttons.html">
-              <i class="bi bi-circle"></i><span>Buttons</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-cards.html">
-              <i class="bi bi-circle"></i><span>Cards</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-carousel.html">
-              <i class="bi bi-circle"></i><span>Carousel</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-list-group.html">
-              <i class="bi bi-circle"></i><span>List group</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-modal.html">
-              <i class="bi bi-circle"></i><span>Modal</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-tabs.html">
-              <i class="bi bi-circle"></i><span>Tabs</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-pagination.html">
-              <i class="bi bi-circle"></i><span>Pagination</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-progress.html">
-              <i class="bi bi-circle"></i><span>Progress</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-spinners.html">
-              <i class="bi bi-circle"></i><span>Spinners</span>
-            </a>
-          </li>
-          <li>
-            <a href="components-tooltips.html">
-              <i class="bi bi-circle"></i><span>Tooltips</span>
+        <ul v-if="item.subitens" :id="`${item.nome}-nav`" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li v-for="subitem in item.subitens">
+            <a :href="subitem.rota">
+              <i class="bi bi-circle"></i><span>{{ subitem.nome }}</span>
             </a>
           </li>
         </ul>
-      </li><!-- End Components Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="forms-elements.html">
-              <i class="bi bi-circle"></i><span>Form Elements</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-layouts.html">
-              <i class="bi bi-circle"></i><span>Form Layouts</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-editors.html">
-              <i class="bi bi-circle"></i><span>Form Editors</span>
-            </a>
-          </li>
-          <li>
-            <a href="forms-validation.html">
-              <i class="bi bi-circle"></i><span>Form Validation</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Forms Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>General Tables</span>
-            </a>
-          </li>
-          <li>
-            <a href="tables-data.html">
-              <i class="bi bi-circle"></i><span>Data Tables</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Tables Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="charts-chartjs.html">
-              <i class="bi bi-circle"></i><span>Chart.js</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-apexcharts.html">
-              <i class="bi bi-circle"></i><span>ApexCharts</span>
-            </a>
-          </li>
-          <li>
-            <a href="charts-echarts.html">
-              <i class="bi bi-circle"></i><span>ECharts</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Charts Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="icons-bootstrap.html">
-              <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-remix.html">
-              <i class="bi bi-circle"></i><span>Remix Icons</span>
-            </a>
-          </li>
-          <li>
-            <a href="icons-boxicons.html">
-              <i class="bi bi-circle"></i><span>Boxicons</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Icons Nav -->
-
-      <li class="nav-heading">Pages</li>
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
-          <i class="bi bi-person"></i>
-          <span>Profile</span>
-        </a>
-      </li><!-- End Profile Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-faq.html">
-          <i class="bi bi-question-circle"></i>
-          <span>F.A.Q</span>
-        </a>
-      </li><!-- End F.A.Q Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact.html">
-          <i class="bi bi-envelope"></i>
-          <span>Contact</span>
-        </a>
-      </li><!-- End Contact Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-register.html">
-          <i class="bi bi-card-list"></i>
-          <span>Register</span>
-        </a>
-      </li><!-- End Register Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-login.html">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-      </li><!-- End Login Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-error-404.html">
-          <i class="bi bi-dash-circle"></i>
-          <span>Error 404</span>
-        </a>
-      </li><!-- End Error 404 Page Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-blank.html">
-          <i class="bi bi-file-earmark"></i>
-          <span>Blank</span>
-        </a>
-      </li><!-- End Blank Page Nav -->
+      </li>
 
     </ul>
 
@@ -240,10 +32,105 @@
 </template>
 
 <script>
-export default {
-  name: 'SideBar'
-}
-</script>
+    var vm = new Vue({
+      el: '#sidebar',
+      data() {
+        return {
+          menu: [
+            {
+              nome: 'Compras',
+              // rota: 'compras.php',
+              subitens: [
+                {
+                  nome: 'Cadastrar Nova Compra',
+                  rota: 'nova-compra.php'
+                },
+                {
+                  nome: 'Lista de Compras',
+                  rota: 'compras.php'
+                },
+              ]
+            },
+            {
+              nome: 'Vendas',
+              // rota: 'compras.php',
+              subitens: [
+                {
+                  nome: 'Cadastrar Nova Venda',
+                  rota: 'nova-venda.php'
+                },
+                {
+                  nome: 'Lista de Vendas',
+                  rota: 'vendas.php'
+                },
+              ]
+            },
+            {
+              nome: 'Parcelamentos',
+              // rota: 'compras.php',
+              subitens: [
+                {
+                  nome: 'Lista de Parcelamentos',
+                  rota: 'parcelamentos.php'
+                },
+                {
+                  nome: 'Registrar Pagamento de Parcela',
+                  rota: 'nova-pagamento.php'
+                }
+              ]
+            },
+            {
+              nome: 'Clientes',
+              rota: '',
+              subitens: [
+                {
+                  nome: 'Lista de Clientes',
+                  rota: 'clientes.php'
+                },
+                {
+                  nome: 'Cadastrar Novo Cliente',
+                  rota: 'novo-cliente.php'
+                }
+              ],
+            },
+            {
+              nome: 'Fornecedores',
+              rota: '',
+              subitens: [
+                {
+                  nome: 'Lista de Fornecedores',
+                  rota: 'fornecedores.php'
+                },
+                {
+                  nome: 'Cadastrar Novo Fornecedor',
+                  rota: 'novo-fornecedor.php'
+                }
+              ],
+            },
+            {
+              nome: 'Produtos',
+              rota: '',
+              subitens: [
+                {
+                  nome: 'Lista de Produtos',
+                  rota: 'produtos.php'
+                },
+                {
+                  nome: 'Cadastrar Novo Produto',
+                  rota: 'novo-produto.php'
+                }
+              ]
+            }
+          ]
+        }
+      },
+      mounted() {
+        console.warn('oi')
+      },
+      methods: {
+      },
+    })
+  </script>
 
 <style>
 
